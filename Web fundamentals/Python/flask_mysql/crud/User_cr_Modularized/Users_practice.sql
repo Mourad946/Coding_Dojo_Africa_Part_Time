2aefab01cc5f671c6faa216ab2177d27c77a0304
Mourad946 <mouradthlijeni@gmail.com>
2024-06-28 22:50:54 +0100

    Python

---
+++ Web fundamentals/Python/flask_mysql/crud/User_cr_Modularized/Users_practice.sql
@@ -0,0 +1,11 @@
+SELECT * FROM users_schema.users;
+INSERT INTO users (first_name, last_name, email) VALUES ('Mourad', 'Thlijeni','mouradthlijeni@gmail');
+INSERT INTO users (first_name, last_name, email) VALUES ('Will', 'Pharell', 'Will.pharell@gmail.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Tom', 'Brady','Tom.brady@gmail.com');
+SELECT * FROM users;
+SELECT * FROM users WHERE email = 'mouradthlijeni@gmail';
+SELECT * FROM users ORDER BY id DESC LIMIT 1;
+UPDATE users SET last_name = 'Pancakes' WHERE id = 3;
+DELETE FROM users WHERE id = 2;
+SELECT * FROM users ORDER BY first_name;
+SELECT * FROM users ORDER BY first_name DESC;
